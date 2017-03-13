950fa9cb640eac0cde8cac4f9b5e3d848759d107
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-03-13 22:55:40 +0100

    Use the company logo style

--- app/src/main/java/com/example/android/singlescreen/MainActivity.java
+++ app/src/main/java/com/example/android/singlescreen/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.singlescreen;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Typeface typeface=Typeface.createFromAsset(getAssets(), "fonts/Montserrat-Regular.ttf");
+
+        TextView businessNameTextView = (TextView)findViewById(R.id.tv_business_name);
+        businessNameTextView.setTypeface(typeface);
     }
 }
