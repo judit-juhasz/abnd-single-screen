f4e2b772641a789b7e95afb2d9794b947dc91b77
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-03-14 00:03:08 +0100

    Make the contact links interactive

--- app/src/main/java/com/example/android/singlescreen/MainActivity.java
+++ app/src/main/java/com/example/android/singlescreen/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.android.singlescreen;
 
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import static android.R.id.message;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +17,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Typeface typeface=Typeface.createFromAsset(getAssets(), "fonts/Montserrat-Regular.ttf");
+        final Typeface typeface=Typeface.createFromAsset(getAssets(),
+                "fonts/Montserrat-Regular.ttf");
 
         TextView businessNameTextView = (TextView)findViewById(R.id.tv_business_name);
         businessNameTextView.setTypeface(typeface);
+
+        final TextView websiteTextView = (TextView)findViewById(R.id.tv_website);
+        websiteTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openWebsite = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.tanits.com"));
+                startActivity(openWebsite);
+            }
+        });
+
+        final TextView twitterTextView = (TextView)findViewById(R.id.tv_twitter);
+        twitterTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openWebsite = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.twitter.com/TanitsHQ"));
+                startActivity(openWebsite);
+            }
+        });
+
+        final TextView linkedinTextView = (TextView)findViewById(R.id.tv_linkedin);
+        linkedinTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openWebsite = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://www.linkedin.com/company/tanits"));
+                startActivity(openWebsite);
+            }
+        });
     }
 }
